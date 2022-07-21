33d44aadcf6f4a36ff530dc10a1e62822d44bfc0
Rafal Zw <zwierz.rafal@gmail.com>
2022-07-21 23:01:55 +0200

    Added auth module.

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Post, Body, Res } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Response } from 'express';
+import { AuthLoginDto } from './dto/auth-login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/login')
+  async userLogin(
+    @Body() req: AuthLoginDto,
+    @Res() res: Response,
+  ): Promise<any> {
+    return this.authService.login(req, res);
+  }
+}
