1322eb1deb795a6c0e499095ba46278a0cb6326f
Rafal Zw <zwierz.rafal@gmail.com>
2022-07-23 00:51:03 +0200

    Added Role decorator.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,17 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Inject, Post, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { RegisterAdminDto } from './dto/register-admin.dto';
-import { registerUserResponse } from '../types/user';
+import { registerUserResponse, UserRole } from '../types/user';
+import { Role } from '../decorators/user-role.decorator';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('user')
 export class UserController {
   constructor(@Inject(UserService) private userService: UserService) {}
 
   @Post('/register')
+  @UseGuards(AuthGuard('jwt'))
+  @Role(UserRole.ADMIN)
   register(@Body() newUser: RegisterAdminDto): Promise<registerUserResponse> {
     return this.userService.register(newUser);
   }
