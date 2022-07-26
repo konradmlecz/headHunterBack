0c14429539da860ba8d578af32ebcc30771422d8
Rafal Zw <zwierz.rafal@gmail.com>
2022-07-26 21:13:02 +0200

    Created getStudentProfile endpoint.

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UserRoleGuard } from '../guards/user-role.guard';
+import { Role } from '../decorators/user-role.decorator';
+import { UserRole } from '../types/user';
+import { StudentService } from './student.service';
+import { UserObj } from '../decorators/user-obj.decorator';
+import { User } from 'src/user/user.entity';
+import { Student } from '../types/student';
 
 @Controller('student')
-export class StudentController {}
+export class StudentController {
+  constructor(@Inject(StudentService) private studentService: StudentService) {}
+
+  @Get('/profile')
+  @UseGuards(AuthGuard('jwt'), UserRoleGuard)
+  @Role(UserRole.STUDENT)
+  getStudentProfile(@UserObj() student: User): Student {
+    return this.studentService.getProfile(student);
+  }
+}
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '../user/user.entity';
+import { Student } from '../types/student';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  getProfile(student: User): Student {
+    const {
+      id,
+      email,
+      phone,
+      firstName,
+      lastName,
+      githubUsername,
+      portfolioUrls,
+      projectUrls,
+      bio,
+      expectedTypeWork,
+      targetWorkCity,
+      expectedContractType,
+      expectedSalary,
+      canTakeApprenticeship,
+      monthsOfCommercialExp,
+      education,
+      workExperience,
+      courses,
+    } = student;
+
+    return {
+      id,
+      email,
+      phone,
+      firstName,
+      lastName,
+      githubUsername,
+      portfolioUrls,
+      projectUrls,
+      bio,
+      expectedTypeWork,
+      targetWorkCity,
+      expectedContractType,
+      expectedSalary,
+      canTakeApprenticeship,
+      monthsOfCommercialExp,
+      education,
+      workExperience,
+      courses,
+    };
+  }
+}
