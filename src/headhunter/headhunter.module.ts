8d15efaf33b3763f0aaef67e8e20b2333e994941
adamkujawski <53292274+adamkujawski@users.noreply.github.com>
2022-07-27 15:39:38 +0200

    Revert "#9 be email module"

--- src/headhunter/headhunter.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HeadhunterController } from './headhunter.controller';
-import { HeadhunterService } from './headhunter.service';
-
-@Module({
-  controllers: [HeadhunterController],
-  providers: [HeadhunterService]
-})
-export class HeadhunterModule {}
