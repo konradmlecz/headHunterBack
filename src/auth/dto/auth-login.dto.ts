d3a86f336d1622abe4d8b38de0375cb958189caf
Rafal Zw <zwierz.rafal@gmail.com>
2022-07-28 20:58:06 +0200

    Fixed cors settings.

--- src/auth/dto/auth-login.dto.ts
+++ src/auth/dto/auth-login.dto.ts
@@ -1,11 +1,8 @@
 import { IsString } from 'class-validator';
-import { UserRole } from '../../types/user';
 
 export class AuthLoginDto {
   @IsString()
   email: string;
   @IsString()
   pwd: string;
-  @IsString()
-  role: UserRole;
 }
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import { config } from './config/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+  app.enableCors({ origin: config.corsOrigin, credentials: true });
   app.useGlobalPipes(
     new ValidationPipe({
       // disableErrorMessages: true,
