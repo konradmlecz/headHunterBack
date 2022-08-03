b32b24df2763768f963dca1214590b943a2833df
Wojtek <rossa21@gmail.com>
2022-08-03 14:35:48 +0200

    Added CRON logic.

--- src/cron/cron.module.ts
+++ src/cron/cron.module.ts
@@ -3,7 +3,7 @@ import {CronService} from './cron.service';
 import {ScheduleModule} from "@nestjs/schedule";
 
 @Module({
-    imports: [ScheduleModule.forRoot()],
+    imports: [ ScheduleModule.forRoot()],
     providers: [CronService]
 })
 export class CronModule {
--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -1,11 +1,36 @@
 import { Injectable } from '@nestjs/common';
-import {Cron, CronExpression} from "@nestjs/schedule";
+import { Cron } from "@nestjs/schedule";
+import { User } from "../user/user.entity";
+import { StudentStatus } from "../types/user";
+import { StudentToInterview } from "../student/student-to-interview.entity";
 
 @Injectable()
 export class CronService {
 
-    // @Cron(CronExpression.EVERY_5_SECONDS)
-    // hello(){
-    //     console.log('Hi');
-    // }
+    @Cron("0 */12 * * *")
+    async updateStatus() {
+
+        const tenDays = 1000 * 60 * 60 * 24 * 10;
+
+        const studentsToInterview = await StudentToInterview.find();
+
+        if (studentsToInterview.length !== 0) {
+
+            studentsToInterview
+                .filter(student => +student.createdAt + tenDays < +new Date())
+                .map(async student => {
+
+                    const foundStudent = await User.findOne({where: {id: student.studentId}});
+                    foundStudent.status = StudentStatus.AVAILABLE;
+                    foundStudent.headHunter = null;
+                    await foundStudent.save();
+
+                    const foundStatus = await StudentToInterview.findOne({where: {studentId: student.studentId}});
+                    await foundStatus.remove();
+                });
+
+        }
+
+    }
+
 }
