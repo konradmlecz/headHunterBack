29f49d49da51c56bfa06d86694339e782a316327
adam kujawski <a.kujawski90@gmail.com>
2022-08-10 00:26:20 +0200

    - change url in email template

--- src/templates/email/register.ts
+++ src/templates/email/register.ts
@@ -2,7 +2,7 @@ export const registerEmailTemplate = (id: string, token: string) => {
   return `
     <h1>Witaj na platfromie Head Hunter MEGAK!</h1>
     <p>Twoje konto zostało założone! Kliknij w poniższy link, aby potwierdzić rejestracje i ustawić hasło!</p>
-    <p><a href="https://www.youtube.com/watch?v=6iFbuIpe68k" target="_blank">http://localhost:3001/user/register/${id}/${token}</a></p>
+    <p><a href="http://localhost:3000/user/register/${id}/${token}" target="_blank">http://localhost:3001/user/register/${id}/${token}</a></p>
     
     <p>Pozdrawiamy,</p>;
     <p>Zespoł MEGAK</p>`;
