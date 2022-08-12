d579e654e7d0ec8407e5c305270e52b7f19b9586
Rafal Zw <zwierz.rafal@gmail.com>
2022-08-12 19:25:41 +0200

    Updated registerEmailTemplate.

--- src/templates/email/register.ts
+++ src/templates/email/register.ts
@@ -2,8 +2,8 @@ export const registerEmailTemplate = (id: string, token: string) => {
   return `
     <h1>Witaj na platfromie Head Hunter MEGAK!</h1>
     <p>Twoje konto zostało założone! Kliknij w poniższy link, aby potwierdzić rejestracje i ustawić hasło!</p>
-    <p><a href="http://localhost:3000/user/register/${id}/${token}" target="_blank">http://localhost:3001/user/register/${id}/${token}</a></p>
+    <p><a href="https://headhunter-g3.networkmanager.pl/user/register/${id}/${token}" target="_blank">https://headhunter-g3.networkmanager.pl/user/register/${id}/${token}</a></p>
     
-    <p>Pozdrawiamy,</p>;
+    <p>Pozdrawiamy</p>;
     <p>Zespoł MEGAK</p>`;
 };
